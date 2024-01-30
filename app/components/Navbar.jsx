4be9349815f13747d59c75b39cf8201d8020a3a9
HMT\hpone <hponemy8tthu@gmail.com>
2024-01-30 14:14:01 +0630

    [ add ] design about dash board page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "./dojo-logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Doji Helpdesk</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
-    </div>
-  )
-}
+    <nav>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk logo"
+        width={70}
+        placeholder="blur"
+        quality={100}
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
